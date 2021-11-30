226e14a9236a203aa98b52268afa593fe7838328
Paatz <patrick.spisak@gmx.at>
2021-11-30 10:37:19 +0100

    project - event bus created

---
+++ vertx-quickstart/src/main/java/org/acme/vertx/GreetingService.java
@@ -0,0 +1,14 @@
+package org.acme.vertx;
+
+import io.quarkus.vertx.ConsumeEvent;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class GreetingService {
+
+    @ConsumeEvent
+    public String consume(String name) {
+        return name.toUpperCase();
+    }
+}
